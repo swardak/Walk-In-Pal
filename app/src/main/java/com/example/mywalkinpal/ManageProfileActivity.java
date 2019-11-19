17d54a1a5b790d2aa74174c2d7f45b2f5de71516
MLuyao Wang <lwang1309@gmail.com>
2019-11-19 16:06:39 -0500

    fix directory issue

--- app/src/main/java/com/example/mywalkinpal/ManageProfileActivity.java
+++ app/src/main/java/com/example/mywalkinpal/ManageProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.mywalkinpal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -60,7 +61,9 @@ public class ManageProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(validate()){
                     update();
-                    finish();
+                    startActivity(new Intent(ManageProfileActivity.this, ViewProfileActivity.class));
+
+                    //finish();
                 }
 
 
--- app/src/main/java/com/example/mywalkinpal/ViewProfileActivity.java
+++ app/src/main/java/com/example/mywalkinpal/ViewProfileActivity.java
@@ -1,14 +1,37 @@
 package com.example.mywalkinpal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ViewProfileActivity extends AppCompatActivity {
+    TextView viewAddress, viewName, viewNumber, viewInsurance, viewPayment;
+    DatabaseReference dbref;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+        viewAddress = findViewById(R.id.viewAddress);
+        viewName = findViewById(R.id.viewName);
+        viewNumber = findViewById(R.id.viewPhoneNumber);
+        viewInsurance = findViewById(R.id.viewInsurance);
+        viewPayment = findViewById(R.id.viewPayment);
+
+        dbref = FirebaseDatabase.getInstance().getReference("Users");
+        Intent intent = getIntent();
+
+        viewAddress.setText(intent.getStringExtra("address"));
+        viewName.setText(intent.getStringExtra("clinicName"));
+        viewNumber.setText(intent.getStringExtra("phoneNumber"));
+        viewInsurance.setText(intent.getStringExtra("insuranceTypesAccepted"));
+        viewPayment.setText(intent.getStringExtra("paymentTypesAccepted"));
+
     }
 }
